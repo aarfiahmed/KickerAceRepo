db6abec1fd89a63630347f881701f102bfa51f70
AARFI AHMED <AARFI.MIRACLE@GMAIL.COM>
2017-10-28 20:36:31 +0530

    Initial commit.

---
+++ KickerAceProject/src/main/java/com/app/KickerAceProject/KickerAceProjectApplication.java
@@ -0,0 +1,12 @@
+package com.app.KickerAceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickerAceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickerAceProjectApplication.class, args);
+	}
+}
